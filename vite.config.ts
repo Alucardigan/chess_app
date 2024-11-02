ff96e41ff6d164bfd8fd477cdfc542551fbc0ed5
Alucardigan <thejas.t.vinod@gmail.com>
2024-11-02 16:49:06 +1100

    fixed proxy issue

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,13 @@ import { defineConfig
     // Load env file based on `mode` in the current working directory.
     // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
     const env= loadEnv(mode, process.cwd(), '')
-    console.log(env)
     return defineConfig({
       server: {
           host: "0.0.0.0",
           port: 3000,
           proxy: {
               "/api": {
-                  target: env.VITE_BACKEND_URL,
+                  target: env.VITE_BACKEND_TARGET,
                   changeOrigin: true
               }
           }
