00d58c2f16bceb411e57063f6ed8846876813860
Alucardigan <thejas.t.vinod@gmail.com>
2024-11-08 14:11:44 +1100

    added client socket and pushes socket functions down to children nodes

--- src/Pages/ChessPage.tsx
+++ src/Pages/ChessPage.tsx
@@ -1,18 +1,36 @@
 
 import { useParams } from "react-router-dom"
 import ChessBoard from "../Components/Chessboard"
-import { useState } from "react"
-
+import { useEffect, useRef, useState } from "react"
+import { Socket, io } from "socket.io-client";
 
 function ChessPage(){
     //get the game state from game ID via request to backend
     const {gameID} = useParams()
-    const [boardState,setBoardState] = useState("RNBQKBNRPPPPPPPP................................pppppppprnbkqbnr")
-    
+    const [boardState,setBoardState] = useState("rnbqkbnrpppppppp................................PPPPPPPPRNBQKBNR")
+    //socketReferences
+    const socketRef = useRef<Socket|null>(null)
+    useEffect(()=>{
+        socketRef.current = io('http://localhost:8080')
+        socketRef.current.on("connect",()=>{
+            console.log('connected to backend',socketRef.current?.id)
+        })
+        return ()=> {
+            socketRef.current?.disconnect()
+            console.log('disconnected')
+        };
+    },[])
+    //socket events 
+    const onMove =(from:number,to:number)=>{
+        if(socketRef.current){
+            socketRef.current.emit('movePiece',{from,to})
+        }
+
+    }
     return (
     <div>{gameID}
         <div className="chessboard">
-            <ChessBoard boardState={boardState}/>
+            <ChessBoard boardState={boardState} onMove={onMove}/>
         </div>
     </div>
     )
