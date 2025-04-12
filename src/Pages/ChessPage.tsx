e6759843a2c83e47e14c975a6dbcc9ce577fbc24
Alucardigan <thejas.t.vinod@gmail.com>
2025-04-12 19:34:43 +1000

    Chess Page should work with backend using session tokens than local storage

--- src/Pages/ChessPage.tsx
+++ src/Pages/ChessPage.tsx
@@ -7,21 +7,22 @@ import { Socket, io } from "socket.io-client";
 function ChessPage(){
     //get the game state from game ID via request to backend
     const {gameID} = useParams()
-    const userID = localStorage.getItem("chessGameUserID")
+    const userID = (sessionStorage.getItem("chessGameUserID"))
     if(!userID){
         console.log("ERROR: userID doesnt exist")
     }
-    const [boardState,setBoardState] = useState("rnbqkbnrpppppppp................................PPPPPPPPRNBQKBNR")
+    const [boardState,setBoardState] = useState<string>("rnbqkbnrpppppppp................................PPPPPPPPRNBQKBNR")
     //socketReferences
     const socketRef = useRef<Socket|null>(null)
     useEffect(()=>{
         socketRef.current = io('http://localhost:8080')
         socketRef.current.on("connect",()=>{
             console.log('connected to backend',socketRef.current?.id)
+            socketRef.current?.emit("joinGame",{gameID,userID: userID})
         })
-        socketRef.current.on("receiveGame",(newBoardState:string)=>{
+        socketRef.current.on("receiveGame",(newBoardState:{bitString: string,checkmate: boolean , winner : string })=>{
             console.log(newBoardState)
-            setBoardState(newBoardState)
+            setBoardState(newBoardState.bitString)
         })
         return ()=> {
             socketRef.current?.disconnect()
@@ -31,7 +32,8 @@ function ChessPage(){
     //socket events 
     const onMove =(from:number,to:number)=>{
         if(socketRef.current){
-            socketRef.current.emit('movePiece',{gameID,userID,from,to})
+            const userID = (sessionStorage.getItem("chessGameUserID"))
+            socketRef.current.emit('movePiece',{gameID,userID: userID,from,to})
         }
 
     }
